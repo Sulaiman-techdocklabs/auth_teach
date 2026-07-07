25a2ea2945c8b07c3dac4d82c297f25346dbfd9f
Sulaiman-techdocklabs <sulaiman@techdocklabs.com>
2026-07-07 12:49:43 +0530

    middlewerea and routes

---
+++ Backend/src/middlewares/auth.js
@@ -0,0 +1,54 @@
+import { verifyToken } from "../services/token.service";
+import User from "../models/user.model.js";
+export const protect = async (req, res, next) =>{
+    try{
+
+        let token;
+        if(req.headers.authorization && req.headers.authorization.startsWith("Bearer")){}
+
+
+        token = req.headers.authorization.split(" ")[1];
+        if(!token){
+            success:false
+            res.status(401).json({message:"Not authorized no token"});
+        }
+
+        const decoded = verifyToken(token);
+        if(!decoded){
+            res.status(401).json({message:" invalid token"});
+        }
+
+const user= await User.findById(decoded.id);
+
+  if(!user){
+            res.status(401).json({message:" user not found"});
+        }
+
+        req.user=user;
+next();
+    }catch(error){
+        res.status(401).json({message:"Not authorized"});
+    }
+}
+
+export const verifyVerified =(req,res,next)=>{
+    if(!req.user.isVerified ){
+        res.status(403).json({message:"Account not verified"});
+    }
+
+    next();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Backend/src/routes/auth.routes.js
@@ -0,0 +1,24 @@
+import express from "express";
+import passport from "passport";
+import { protect } from "../middlewares/auth.js";
+import { signup, verifyEmail, login, sendLoginOtp, loginOtpVerify,googleCallback, forgotPassword, resetPassword } from "../controllers/auth.controller.js";
+
+const router = express.Router();
+
+
+router.post("/signup",signup);
+router.get("/verify-email/:token",verifyEmail);
+
+router.post("/login",login);
+router.post("/login-otp/send",sendLoginOtp);
+router.post("/login-otp/verify",loginOtpVerify);
+
+router.get("/google",passport.authenticate("google",{scope:["profile","email"]}));
+router.get("/google/callback",passport.authenticate("google",{failureRedirect:"/login"}),googleCallback);
+
+
+router.post("/forgot-password",forgotPassword);
+router.post("/reset-password/:token",resetPassword);
+
+router.get("/me",protect,getMe);
+router.post("/logout",protect,logout);
